ff1dac89c1fdb8b5c805106dff31dd1968d18dcc
Tobias Donder <tobias.donder@tk.de>
2022-10-25 16:53:30 +0200

    Some code cleanup

--- cuddly-carnival/Views/LevelElement.swift
+++ cuddly-carnival/Views/LevelElement.swift
@@ -7,38 +7,65 @@ struct LevelElement: View {
     private let arcColor: Color = .white
 
     var body: some View {
-        ZStack {
-            Color(.systemBackground)
-            VStack(spacing: 0.0) {
-                GeometryReader { geometry in
-                    ForEach(0 ..< 10) { i in
+
+        VStack(spacing: 0.0) {
+            GeometryReader { geometry in
+                ForEach(0 ..< 10) { i in
+                    ZStack {
                         Arc()
-                            .stroke(style: StrokeStyle(lineWidth: CGFloat(20 - i), lineCap: .round, lineJoin: .round))
+                            .stroke(
+                                style: StrokeStyle(
+                                    lineWidth: CGFloat(20 - i),
+                                    lineCap: .round,
+                                    lineJoin: .round
+                                )
+                            )
                             .foregroundColor(borderColor)
                             .frame(
                                 width: geometry.size.width * ((numberOfArcs - CGFloat(i)) / numberOfArcs),
                                 height: geometry.size.height / numberOfArcs
                             )
-                            .position(x: geometry.size.width / 2, y: CGFloat(i) * (geometry.size.height / numberOfArcs))
+                            .position(
+                                x: geometry.size.width / 2,
+                                y: CGFloat(i) * (geometry.size.height / numberOfArcs)
+                            )
 
                         Arc()
-                            .stroke(style: StrokeStyle(lineWidth: CGFloat(14 - i), lineCap: .round, lineJoin: .round))
+                            .stroke(
+                                style: StrokeStyle(
+                                    lineWidth: CGFloat(14 - i),
+                                    lineCap: .round,
+                                    lineJoin: .round
+                                )
+                            )
                             .foregroundColor(arcColor.opacity(0.3))
                             .frame(
                                 width: geometry.size.width * ((numberOfArcs - CGFloat(i)) / numberOfArcs),
                                 height: geometry.size.height / numberOfArcs
                             )
-                            .position(x: geometry.size.width / 2, y: CGFloat(i) * (geometry.size.height / numberOfArcs))
+                            .position(
+                                x: geometry.size.width / 2,
+                                y: CGFloat(i) * (geometry.size.height / numberOfArcs)
+                            )
                     }
                 }
-
-                Image(systemName: "speaker")
-                    .font(.system(size: 56.0))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(borderColor)
-                    .rotationEffect(Angle(degrees: 270))
             }
+
+            Image(systemName: "speaker")
+                .font(.system(size: 56.0))
+                .multilineTextAlignment(.center)
+                .foregroundColor(borderColor)
+                .rotationEffect(Angle(degrees: 270))
         }
+    }
+
+    private func progress(
+        value: Double,
+        maxValue: Double,
+        height: CGFloat
+    ) -> CGFloat {
+        let percentage = value / maxValue
+        return height - (height * CGFloat(percentage))
     }
 }
 
